7bf84e20efc49f40cb7998750cc82def489b4da5
obrm <obrm770@gmail.com>
2021-05-22 11:16:43 +0300

    refactoring

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -3,7 +3,7 @@ import { all, call } from 'redux-saga/effects'
 import { weatherSagas } from './weather/weatherSagas'
 import { favoritesSagas } from './favorites/favoritesSagas'
 import { autoCompleteSagas } from './autoComplete/autoCompleteSagas'
-import { darkModeSagas } from './darkMode/darkThemeSagas'
+import { darkModeSagas } from './darkMode/darkModeSagas'
 
 export default function* rootSaga() {
   yield all([
