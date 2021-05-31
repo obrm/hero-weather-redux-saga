4df3ae3eb7d1a91711e018c093316116cb8b4e4a
Ori Baram <obrm770@gmail.com>
2021-05-31 09:28:30 +0300

    omitted theme sagas

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -3,13 +3,7 @@ import { all, call } from 'redux-saga/effects'
 import { weatherSagas } from './weather/weatherSagas'
 import { favoritesSagas } from './favorites/favoritesSagas'
 import { autoCompleteSagas } from './autoComplete/autoCompleteSagas'
-import { themeSagas } from './theme/themeSagas'
 
 export default function* rootSaga() {
-  yield all([
-    call(weatherSagas),
-    call(favoritesSagas),
-    call(autoCompleteSagas),
-    call(themeSagas),
-  ])
+  yield all([call(weatherSagas), call(favoritesSagas), call(autoCompleteSagas)])
 }
