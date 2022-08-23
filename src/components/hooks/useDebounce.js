e4ee5a4ab04e06254d15bd99042fb902b2bbae08
Ori Baram <obrm770@gmail.com>
2022-08-23 13:03:22 +0300

    useDebounce

--- src/components/hooks/useDebounce.js
+++ src/components/hooks/useDebounce.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-function useDebounce(value, delay) {
+const useDebounce = (value, delay) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
